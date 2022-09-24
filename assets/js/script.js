8727a148221e3a9190c06c37ef18dd04c463033b
nibir4017 <96000416+nibir4017@users.noreply.github.com>
2022-09-24 19:08:16 +0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,46 @@
 
 
-var birdHeightPos = 50;
 
+ var birdHeightPos = 50;
+ let currentBird;
+ var intervalTime= 2000;
 $(".bird").click(function(){
-    let currentBird = $(this);
+   
+     currentBird = $(this);
     
      currentBird.animate({
         bottom:birdHeightPos + 'px',
-        transition:"bottom 0.2s"
+        transition:"bottom 2s"
      })
     
-   
-    if(birdHeightPos>50){
-        $(this).animate(
-            {bottom: birdHeightPos - 20}
-        )
-    }else{
-        console.log("aint ggreater")
-    }
     birdHeightPos+=50
+    intervalTime+=1000
     console.log(birdHeightPos)
+    console.log(intervalTime +"insideClick")
 }
 )
+// setInterval(function(){ 
+//     console.log("5 secs have passed"); 
+// }, 5000/*5000 ms=5 seconds*/);
 
+setInterval(function (){
+     
+        $(".bird").animate(
+            {bottom: 0},1000
+        )
+    birdHeightPos=50
     
+    console.log(intervalTime +"insideSetInterval")
+},intervalTime)
+  
+
+
+setInterval(function(){
+    var pillarHeight = parseInt(Math.random()*(500-100)+100);
+   $(".pillar").css({
+    "height":pillarHeight +"px"
+   }
+   )
+   console.log(pillarHeight)
+},1000);
+
