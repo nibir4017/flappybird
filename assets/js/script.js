c672416234db028efa9c57e7a7a56b7525c7927c
nibir4017 <96000416+nibir4017@users.noreply.github.com>
2022-10-06 17:45:46 +0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,44 @@
 
+/*Bird Stuff*/ 
+document.addEventListener('DOMContentLoaded' , () => {
+  let bird =  $(".bird") 
+ let birdLeft = 50;
+ let birdBottom = 100;
+ let gravity = 2
 
+function startGame(){
+    birdBottom -= gravity;
+    bird.css({
+        "bottom": birdBottom +"px",
+        "left": birdLeft + "px"
 
- var birdHeightPos = 50;
- let currentBird;
- var intervalTime= 2000;
-$(".bird").click(function(){
-   
-     currentBird = $(this);
-    
-     currentBird.animate({
-        bottom:birdHeightPos + 'px',
-        transition:"bottom 2s"
-     })
-    
-    birdHeightPos+=50
-    intervalTime+=1000
-    console.log(birdHeightPos)
-    console.log(intervalTime +"insideClick")
+    })
 }
-)
+let timerId = setInterval(startGame,20)
+
+function jump(){
+   if(birdBottom<400) birdBottom += 50;
+    bird.animate({
+        "bottom": birdBottom + "px"
+    },5)
+}
+document.addEventListener('click',jump)
+
+
+})
 // setInterval(function(){ 
 //     console.log("5 secs have passed"); 
 // }, 5000/*5000 ms=5 seconds*/);
 
-setInterval(function (){
-     
-        $(".bird").animate(
-            {bottom: 0},1000
-        )
-    birdHeightPos=50
-    
-    console.log(intervalTime +"insideSetInterval")
-},intervalTime)
-  
 
 
-setInterval(function(){
-    var pillarHeight = parseInt(Math.random()*(500-100)+100);
-   $(".pillar").css({
+
+ setInterval(function(){
+     var pillarHeight = parseInt(Math.random()*(500-100)+100);
+    $(".pillar").css({
     "height":pillarHeight +"px"
    }
-   )
-   console.log(pillarHeight)
-},1000);
+    )
+  console.log(pillarHeight)
+ },1000);
 
